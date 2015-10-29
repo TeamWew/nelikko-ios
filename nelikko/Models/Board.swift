5dc4d3933e1b3f42e7d36469d4452c2dc3152f57
Ilari Lind <ilarilind@Ilaris-MacBook-Pro.local>
2015-10-29 23:58:00 +0200

    Board datamodel

--- nelikko/Models/Board.swift
+++ nelikko/Models/Board.swift
@@ -7,3 +7,24 @@
 //
 
 import Foundation
+
+import CoreData
+
+@objc(Board)
+class Board: NSManagedObject {
+    
+    @NSManaged var board: String
+    @NSManaged var bump_limit: Int16
+    @NSManaged var image_limit: Int16
+    @NSManaged var is_archived: Bool
+    @NSManaged var max_comment_chars: Int16
+    @NSManaged var max_filesize: Int16
+    @NSManaged var max_web_filesize: Int16
+    @NSManaged var meta_description: String
+    @NSManaged var pages: Int16
+    @NSManaged var per_page: Int16
+    @NSManaged var title: String
+    @NSManaged var ws_board: Bool
+    
+    @NSManaged var threads: NSSet // Relation
+}
