9dd356ce62350f5494f6bd49f4edbfc188abbce9
Nicolas Arkkila <nicolas@arkkila.fi>
2015-11-01 01:12:28 +0200

    Implemented working board fetch for BoardsViewController

--- nelikko/BoardsViewController.swift
+++ nelikko/BoardsViewController.swift
@@ -7,11 +7,26 @@
 //
 
 import UIKit
+import Alamofire
 
 class BoardsViewController: UITableViewController {
-
+    var boards: Array<String> = []
     override func viewDidLoad() {
         super.viewDidLoad()
+        Alamofire.request(.GET, "https://a.4cdn.org/boards.json")
+            .responseJSON { response in
+                if let JSON = response.result.value {
+                    let things = JSON["boards"] as? NSArray
+                    for item in things! {
+                        let board = item as! Dictionary<String, AnyObject>
+                        let initial: String = board["board"] as! String!
+                        let title: String = board["title"]  as! String!
+                        let boardName: String = "/" + initial + "/ - " + title
+                        self.boards.append(boardName)
+                        self.tableView.reloadData()
+                    }
+                }
+        }
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -21,5 +36,14 @@ class BoardsViewController: UITableViewController {
     }
 
 
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.boards.count
+    }
+
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.textLabel?.text = self.boards[indexPath.row]
+        return cell
+    }
 }
 
