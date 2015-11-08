eab5744e96fc453de6a9cafdef3c04eebd8dc019
Ilari Lind <ilarilind@Ilaris-MacBook-Pro.local>
2015-11-09 00:53:32 +0200

    settings view to thread controller

--- nelikko/SettingsViewController.swift
+++ nelikko/SettingsViewController.swift
@@ -11,9 +11,22 @@ import Foundation
 
 class SettingsViewController: UITableViewController {
     
-    @IBAction func testLabel(sender: AnyObject) {
-        print("test")
-        self.dismissViewControllerAnimated(true, completion: {});
+    @IBAction func dismissModal(sender: UINavigationItem) {
+        if (sender.title == "X") {
+             self.dismissViewControllerAnimated(true, completion: {});
+        }
     }
+    
+    /*
+    @IBAction func saveSwitchState(sender: AnyObject) {
+        var defaults = NSUserDefaults.standardUserDefaults()
+        
+        if bluetoothSwitch.on {
+            defaults.setBool(true, forKey: "SwitchState")
+        } else {
+            defaults.setBool(false, forKey: "SwitchState")
+        }
+    }
+    */
 
 }
