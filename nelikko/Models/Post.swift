6c30bd08e44291d6b8d5c76d4f5f42fc34b9e687
Ilari Lind <ilarilind@Ilaris-MacBook-Pro.local>
2015-11-09 23:18:02 +0200

    add post model and constructor

--- nelikko/Models/Post.swift
+++ nelikko/Models/Post.swift
@@ -8,32 +8,59 @@
 
 import Foundation
 
-import CoreData
-
-@objc(Post)
-class Post: NSManagedObject {
+class Post {
     
-    @NSManaged var no: Int16 // numma
-    @NSManaged var sticky: Bool
-    @NSManaged var closed: Bool
-    @NSManaged var name: String
-    @NSManaged var com: String // comment
-    @NSManaged var filename: String
-    @NSManaged var ext: String
-    @NSManaged var w: Int16
-    @NSManaged var h: Int16
-    @NSManaged var tn_w: Int16 // thumbnail
-    @NSManaged var tn_h: Int16
-    @NSManaged var tim: Int16 // renamed filename epoch
-    @NSManaged var time: Int16 // post epoch
-    @NSManaged var md5: String
-    @NSManaged var fsize: Int16
-    @NSManaged var resto: Int16 // in reply to - 0 is thread OP post
-    @NSManaged var capcode: String // moot string
-    @NSManaged var semantic_url: String
-    @NSManaged var replies: Int16
-    @NSManaged var images: Int16
-    @NSManaged var unique_ips: Int16
+    var no: Int16 // numma
+    var sticky: Bool
+    var closed: Bool
+    var name: String
+    var com: String // comment
+    var filename: String
+    var ext: String
+    var w: Int16
+    var h: Int16
+    var tn_w: Int16 // thumbnail
+    var tn_h: Int16
+    var tim: Int16 // renamed filename epoch
+    var time: Int16 // post epoch
+    var md5: String
+    var fsize: Int16
+    var resto: Int16 // in reply to - 0 is thread OP post
+    var capcode: String // moot string
+    var bumplimit: Bool // bumplimit met
+    var imagelimit: Bool // imagelimit met
+    var semantic_url: String // thread slug
+    var replies: Int16 // reply count
+    var images: Int16 // image count
     
-    @NSManaged var thread: NSSet // Relation
+    init(no:Int16,sticky: Bool, closed: Bool, name: String, com: String, filename: String, ext: String,
+        w: Int16, h: Int16, tn_w: Int16, tn_h: Int16, tim: Int16, time: Int16, md5: String, fsize: Int16,
+        resto: Int16, capcode: String, bumplimit: Bool, imagelimit: Bool, semantic_url: String, replies: Int16, images: Int16) {
+            self.no = no
+            self.sticky = sticky
+            self.closed = closed
+            self.name = name
+            self.com = com
+            self.filename = filename
+            self.ext = ext
+            self.w = w
+            self.h = h
+            self.tn_w = tn_w
+            self.tn_h = tn_h
+            self.tim = tim
+            self.time = time
+            self.md5 = md5
+            self.fsize = fsize
+            self.resto = resto
+            self.capcode = capcode // not found in OP
+            self.bumplimit = bumplimit
+            self.imagelimit = imagelimit
+            self.semantic_url  = semantic_url
+            self.replies = replies
+            self.images = images
+            
+            
+    }
+    
+    
 }
