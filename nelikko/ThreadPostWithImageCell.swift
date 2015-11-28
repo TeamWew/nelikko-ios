2a52e0685c0da36ccb61e16e6dcad33c91b58cfb
Nicolas Arkkila <nicolas@arkkila.fi>
2015-11-28 22:58:03 +0200

    Added ThreadPostWithImageCell as subclass of regular cell

---
+++ nelikko/ThreadPostWithImageCell.swift
@@ -0,0 +1,16 @@
+//
+//  ThreadPostWithImageCell.swift
+//  nelikko
+//
+//  Created by Nicolas Arkkila on 28/11/15.
+//  Copyright © 2015 TeamWew. All rights reserved.
+//
+
+import UIKit
+import Foundation
+
+class ThreadPostWithImageCell : ThreadPostCell {
+    @IBOutlet var postImageView: UIImageView!
+    
+    
+}
